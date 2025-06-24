397c1a5d1ee1b58c71630f3518571cefd515fb8a
Masterkiril <58228824+Masterkiril@users.noreply.github.com>
2025-06-24 22:10:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+    nav.classList.toggle('active');
     burger.classList.toggle('toggle');
 });
 
